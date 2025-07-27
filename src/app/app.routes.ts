6edc5bebad69108c04b87bd02a778a1d79ce9741
rfloyd01 <lynrdskynrd05@gmail.com>
2025-07-27 12:55:31 -0400

    created new GameBoard component and moved all code from Home component into it, cleared code from Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Card } from './components/card/card';
 import { Test} from './components/test/test';
+import { GameBoard } from './components/game-board/game-board';
 
 export const routes: Routes = [
     {path: '', component:Home},
+    {path: 'game', component:GameBoard},
     {path: 'card', component:Card},
     {path: 'test', component:Test}
 ];

4c36451ab748f385be9f7aab35f6e81705087e36
rfloyd01 <lynrdskynrd05@gmail.com>
2025-08-04 09:59:10 -0400

    can now select cards from grid and put into hand, limited at 5

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CARD_TYPES, CardDisplay, CardInfo } from '../../util/card-types';
 import { Card } from '../card/card';
-import { createDefaultStats, createRandomStatsForCardType, randomInteger } from '../../util/card-util';
+import { createRandomStatsForCardType, randomInteger } from '../../util/card-util';
 import { CommonModule } from '@angular/common';
 import { counter } from '../../util/general-utils';
 
@@ -14,16 +14,20 @@ import { counter } from '../../util/general-utils';
 })
 export class Home implements OnInit {
 
-  selectedCards: CardInfo[] = [];
-  selectedCardType!: number | null;
-  selectedCardTypeName!: string | null;
-  highlightedCard!: CardInfo;
-  allCards: (CardInfo | null)[][][] = [];
+  allCards: CardInfo[][][] = [];
+  selectedCards: {info: CardInfo, type: number}[] = []; //selected cards appear on right side of screen, these will be used in the game
 
-  selectedCardsHTML!: (HTMLElement | null);
+  //Fields for Highlighted cards. The highlighted cards appear in the middle of the screen when
+  //the player clicks on a grid square. If there are multiple of a single type of card owned then
+  //these cards can be shuffled through.
+  highlightedCards: CardInfo[] = []; //highlighted cards will appear in center of screen, show clards of currently highlighted grid square
+  highlightedCardType!: number | null;
+  highlightedCardTypeName!: string | null;
+  highlightedCardsHTML!: (HTMLElement | null);
 
+  //Imports
   ALL_CARD_TYPES = CARD_TYPES;
-
+  cardDisplay = CardDisplay;
   counter = counter;
 
   constructor(private router: Router, private route: ActivatedRoute) {
@@ -31,28 +35,13 @@ export class Home implements OnInit {
   }
 
   ngOnInit(): void {
-    //Regardless of what cards the player has, the grid needs a count of each type of card
-    //to properly create itself, so create empty arrays for all 100 card types.
+    //Create the necessary arrays to hold player cards
     for (let i:number = 0; i < 10; i++) {
       this.allCards.push([]);
       for (let j:number = 0; j < 10; j++) {
         this.allCards[i].push([]);
       }
     }
-
-    //For now, generate some random cards to go into the selected
-    //cards array
-    for (let i: number = 0; i < 5; i++) {
-      this.selectedCards.push({
-        id: i,
-        cardStats: createDefaultStats(),
-        isSelected: false,
-        cardDisplay: CardDisplay.FRIEND,
-        cardText: ''
-      });
-    }
-
-    this.highlightedCard = this.selectedCards[0];
 
     this.loadPlayerCards();
   }
@@ -70,7 +59,7 @@ export class Home implements OnInit {
       const row = randomInteger(10);
       const col = randomInteger(10);
       this.allCards[row][col].push({
-        id: i,
+        id: 10 * col + row,
         cardStats: createRandomStatsForCardType(this.ALL_CARD_TYPES[10 * col + row]), //need to transpose row and column to match grid
         isSelected: false,
         cardDisplay: CardDisplay.FRIEND,
@@ -79,7 +68,80 @@ export class Home implements OnInit {
     }
   }
 
+  highlightGridCard(row: number, col: number) {
+    if (this.allCards[row][col].length > 0) {
+      this.highlightedCardType = 10 * col + row; //need to transpose row and column to match grid
+      this.highlightedCardTypeName = this.ALL_CARD_TYPES[this.highlightedCardType].name;
+
+      if (this.allCards[row][col]) {
+        this.highlightedCards = this.allCards[row][col];
+      }
+
+      //Apply the selected CSS class to the grid card in question, and remove it from
+      //any existing selected grid card
+      if (this.highlightedCardsHTML) {
+        this.highlightedCardsHTML.classList.remove('selected');
+      }
+
+      this.highlightedCardsHTML = document.getElementById('grid-card-' + (10 * row + col)); //non-transposed id
+      if (this.highlightedCardsHTML) {
+        this.highlightedCardsHTML.classList.add('selected');
+      }
+    }
+  }
+
+  selectHighlightedCard(index: number) {
+    //When a player clicks on the top card in the highlighted cards array
+    //it will move the card into the selected cards portion of the screen.
+    if (this.selectedCards.length >= 5) {
+      return; //can't select more than 5 cards
+    }
+
+    if (index == 0) {
+      this.selectedCards.push({
+        info: {
+          id: this.selectedCards.length,
+          cardStats: this.highlightedCards[0].cardStats,
+          isSelected: false,
+          cardDisplay: CardDisplay.FRIEND,
+          cardText: ''
+        },
+        type: this.highlightedCards[0].id
+      })
+
+      //The selected card needs to be removed from the grid, and the highlighted
+      //cards array.
+      const col = this.highlightedCards[0].id % 10;
+      const row = Math.floor(this.highlightedCards[0].id / 10);
+      this.highlightedCards = this.highlightedCards.length > 1 ? this.highlightedCards.slice(1) : [];
+      this.allCards[col][row] = this.highlightedCards;
+
+      //If there are no more of the given card type then the text in the highlighted
+      //card section needs to be hidden (contains card number and name) and the image
+      //for the given card in the grid needs to change to the empty slot.
+      if (this.highlightedCards.length == 0) {
+        this.updateBoardPieceImage(row, col);
+        if (this.highlightedCardsHTML != null) {
+          this.highlightedCardsHTML.classList.remove('selected');
+          this.highlightedCardsHTML = null;
+          this.highlightedCardType = null;
+          this.highlightedCardTypeName = null;
+        }
+      }
+    }
+  }
+
+  removeSelectedCard(index: number) {
+    //removes the selected card from the player's hand and puts
+    //it back into the grid and highlighted cards (if necessary).
+    console.log(this.selectedCards[index].type);
+  }
+
   getBoardPieceImage(row: number, col: number) {
+    if (this.allCards[row][col].length == 0) {
+      return 'assets/empty_board_piece.png';
+    }
+
     const multi_string = this.allCards[row][col].length > 1 ? 'multi_' : '';
     const index = (10 * col + row);
     const prefix = 'assets/' + multi_string;
@@ -93,24 +155,12 @@ export class Home implements OnInit {
     }
   }
 
-  selectCardType(row: number, col: number) {
-    if (this.allCards[row][col].length > 0) {
-      this.selectedCardType = 10 * col + row; //need to transpose row and column to match grid
-      this.selectedCardTypeName = this.ALL_CARD_TYPES[this.selectedCardType].name;
-      if (this.allCards[row][col][0] != null) {
-        //Update stats for the card
-        this.highlightedCard.cardStats = this.allCards[row][col][0].cardStats;
-      }
-
-      //Apply the selected CSS class to the grid card in question, and remove it from
-      //any existing selected grid card
-      if (this.selectedCardsHTML) {
-        this.selectedCardsHTML.classList.remove('selected');
-      }
-
-      this.selectedCardsHTML = document.getElementById('grid-card-' + (10 * row + col)); //non-transposed id
-      if (this.selectedCardsHTML) {
-        this.selectedCardsHTML.classList.add('selected');
+  updateBoardPieceImage(row: number, col: number) {
+    if (this.highlightedCardsHTML != null) {
+      let imgChild = this.highlightedCardsHTML.children.item(0);
+      if (imgChild != null) {
+        let imgHTML: HTMLImageElement = imgChild as HTMLImageElement;
+        imgHTML.src = this.getBoardPieceImage(row, col);
       }
     }
   }
